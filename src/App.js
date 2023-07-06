eeb4ba629fb89b9036097a68c9933525081e0e00
Kilas-aniso <kilasaniso@gmail.com>
2023-07-06 06:40:00 +0300

    created first react project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Introduction from './Introduction';
+function App() {
+  return (
+    <div className="App">
+ <Introduction/>
+ <Introduction/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Introduction/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import './style.css'
+const Introduction = () =>{
+return (
+    <div>
+        <h1 className="heading">What I have learnt about ReactJs</h1>
+        <p>It has reusable components</p>
+        <p>The writing format is JSX</p>
+        <p>It is fast</p>
+
+
+
+    </div>
+);
+};
+
+export default Introduction;
